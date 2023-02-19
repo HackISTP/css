b9a5e46d162382308e5424066e7ee3fc5d9d2c3c
HackISTP <hyeminpak@gmail.com>
2023-02-20 01:50:15 +0900

    postcss 와 style-components 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button1 from "./components/Button1";
+import Button2 from "./components/Button2";
+import styled, { css } from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button1 />
+      <Button2 />
+      <Container>
+        <Button>normal버튼</Button>
+        <Button primary>primary 버튼</Button>
+      </Container>
     </div>
   );
 }
 
 export default App;
+const Container = styled.div`
+  display: flex;
+`;
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid #4c5b69;
+  color: #b9eaff;
+  margin: 0.1em;
+  padding: 0.25em 1em;
+  ${(props) =>
+    props.primary &&
+    css`
+      background: #009cd5;
+      color: white;
+    `}
+`;
